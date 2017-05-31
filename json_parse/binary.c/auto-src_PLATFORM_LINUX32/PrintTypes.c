60a480f4b7f3a1d0e30319a7e3a19b50f57f9280
mdashnyam <muugii02@gmail.com>
2017-05-31 16:13:15 +0200

    h

--- json_parse/binary.c/auto-src_PLATFORM_LINUX32/PrintTypes.c
+++ json_parse/binary.c/auto-src_PLATFORM_LINUX32/PrintTypes.c
@@ -10,66 +10,6 @@ static pthread_mutex_t g_printing_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 #endif
 
-void PrintT_UInt32(const char *paramName, const asn1SccT_UInt32 *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    #if WORD_SIZE==8
-    printf("%s %lld\n", paramName, (*pData));
-    #else
-    printf("%s %d\n", paramName, (*pData));
-    #endif
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintT_Boolean(const char *paramName, const asn1SccT_Boolean *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    printf("%s %d\n", paramName, (int)(*pData));
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintMyEnum(const char *paramName, const asn1SccMyEnum *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    printf("%s %d\n", paramName, (int)(*pData));
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintT_Int32(const char *paramName, const asn1SccT_Int32 *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    #if WORD_SIZE==8
-    printf("%s %lld\n", paramName, (*pData));
-    #else
-    printf("%s %d\n", paramName, (*pData));
-    #endif
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
 void PrintMySeqOf(const char *paramName, const asn1SccMySeqOf *pData)
 {
 #ifdef __linux__
@@ -82,32 +22,6 @@ void PrintMySeqOf(const char *paramName, const asn1SccMySeqOf *pData)
             printf("%s::Elem %d\n", paramName, (int)(*pData).arr[i1]);
         }
     }
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintMyBool(const char *paramName, const asn1SccMyBool *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    printf("%s %d\n", paramName, (int)(*pData));
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintMyReal(const char *paramName, const asn1SccMyReal *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    printf("%s %f\n", paramName, (*pData));
 #endif
 #ifdef __linux__
     pthread_mutex_unlock(&g_printing_mutex);
@@ -142,30 +56,7 @@ void PrintMyChoice(const char *paramName, const asn1SccMyChoice *pData)
 #endif
 }
 
-void PrintMySeq(const char *paramName, const asn1SccMySeq *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    #if WORD_SIZE==8
-    printf("%s::input_data %lld\n", paramName, (*pData).input_data);
-    #else
-    printf("%s::input_data %d\n", paramName, (*pData).input_data);
-    #endif
-    #if WORD_SIZE==8
-    printf("%s::output_data %lld\n", paramName, (*pData).output_data);
-    #else
-    printf("%s::output_data %d\n", paramName, (*pData).output_data);
-    #endif
-    printf("%s::validity %d\n", paramName, (int)(*pData).validity);
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintT_Int8(const char *paramName, const asn1SccT_Int8 *pData)
+void PrintT_UInt32(const char *paramName, const asn1SccT_UInt32 *pData)
 {
 #ifdef __linux__
     pthread_mutex_lock(&g_printing_mutex);
@@ -182,7 +73,57 @@ void PrintT_Int8(const char *paramName, const asn1SccT_Int8 *pData)
 #endif
 }
 
+void PrintMyReal(const char *paramName, const asn1SccMyReal *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    printf("%s %f\n", paramName, (*pData));
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
 void PrintMyInteger(const char *paramName, const asn1SccMyInteger *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    #if WORD_SIZE==8
+    printf("%s %lld\n", paramName, (*pData));
+    #else
+    printf("%s %d\n", paramName, (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintMyOctStr(const char *paramName, const asn1SccMyOctStr *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    {
+        int i;
+        printf("%s ", paramName);
+        for(i=0; i<3; i++)
+            printf("%c", (*pData).arr[i]);
+        printf("\n");
+    }
+
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintT_Int32(const char *paramName, const asn1SccT_Int32 *pData)
 {
 #ifdef __linux__
     pthread_mutex_lock(&g_printing_mutex);
@@ -216,20 +157,79 @@ void PrintT_UInt8(const char *paramName, const asn1SccT_UInt8 *pData)
 #endif
 }
 
-void PrintMyOctStr(const char *paramName, const asn1SccMyOctStr *pData)
+void PrintT_Int8(const char *paramName, const asn1SccT_Int8 *pData)
 {
 #ifdef __linux__
     pthread_mutex_lock(&g_printing_mutex);
 #endif
 #ifdef __unix__
-    {
-        int i;
-        printf("%s ", paramName);
-        for(i=0; i<3; i++)
-            printf("%c", (*pData).arr[i]);
-        printf("\n");
-    }
+    #if WORD_SIZE==8
+    printf("%s %lld\n", paramName, (*pData));
+    #else
+    printf("%s %d\n", paramName, (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
 
+void PrintMyEnum(const char *paramName, const asn1SccMyEnum *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    printf("%s %d\n", paramName, (int)(*pData));
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintMyBool(const char *paramName, const asn1SccMyBool *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    printf("%s %d\n", paramName, (int)(*pData));
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintT_Boolean(const char *paramName, const asn1SccT_Boolean *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    printf("%s %d\n", paramName, (int)(*pData));
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintMySeq(const char *paramName, const asn1SccMySeq *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    #if WORD_SIZE==8
+    printf("%s::input_data %lld\n", paramName, (*pData).input_data);
+    #else
+    printf("%s::input_data %d\n", paramName, (*pData).input_data);
+    #endif
+    #if WORD_SIZE==8
+    printf("%s::output_data %lld\n", paramName, (*pData).output_data);
+    #else
+    printf("%s::output_data %d\n", paramName, (*pData).output_data);
+    #endif
+    printf("%s::validity %d\n", paramName, (int)(*pData).validity);
 #endif
 #ifdef __linux__
     pthread_mutex_unlock(&g_printing_mutex);
--- json_parse/binary.c/auto-src_PLATFORM_LINUX32/PrintTypesAsASN1.c
+++ json_parse/binary.c/auto-src_PLATFORM_LINUX32/PrintTypesAsASN1.c
@@ -11,100 +11,6 @@ static pthread_mutex_t g_printing_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 #endif
 
-void PrintASN1MyInteger(const char *paramName, const asn1SccMyInteger *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    //printf("%s MyInteger ::= ", paramName);
-    printf("%s ", paramName);
-    #if WORD_SIZE==8
-    printf("%lld", (*pData));
-    #else
-    printf("%d", (*pData));
-    #endif
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintASN1T_Int32(const char *paramName, const asn1SccT_Int32 *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    //printf("%s T-Int32 ::= ", paramName);
-    printf("%s ", paramName);
-    #if WORD_SIZE==8
-    printf("%lld", (*pData));
-    #else
-    printf("%d", (*pData));
-    #endif
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintASN1T_Boolean(const char *paramName, const asn1SccT_Boolean *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    //printf("%s T-Boolean ::= ", paramName);
-    printf("%s ", paramName);
-    printf("%s", (int)(*pData)?"TRUE":"FALSE");
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintASN1MySeq(const char *paramName, const asn1SccMySeq *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    //printf("%s MySeq ::= ", paramName);
-    printf("%s ", paramName);
-    printf("{");
-    printf("input-data ");
-    #if WORD_SIZE==8
-    printf("%lld", (*pData).input_data);
-    #else
-    printf("%d", (*pData).input_data);
-    #endif
-    printf(", ");
-    printf("output-data ");
-    #if WORD_SIZE==8
-    printf("%lld", (*pData).output_data);
-    #else
-    printf("%d", (*pData).output_data);
-    #endif
-    printf(", ");
-    printf("validity ");
-    switch((*pData).validity) {
-    case 0:
-        printf("valid");
-        break;
-    case 1:
-        printf("invalid");
-        break;
-    default:
-        printf("Invalid value in ENUMERATED ((*pData).validity)");
-    }
-    printf("}");
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
 void PrintASN1MyOctStr(const char *paramName, const asn1SccMyOctStr *pData)
 {
 #ifdef __linux__
@@ -121,6 +27,63 @@ void PrintASN1MyOctStr(const char *paramName, const asn1SccMyOctStr *pData)
         printf("'H");
     }
 
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1MyEnum(const char *paramName, const asn1SccMyEnum *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s MyEnum ::= ", paramName);
+    printf("%s ", paramName);
+    switch((*pData)) {
+    case 0:
+        printf("hello");
+        break;
+    case 1:
+        printf("world");
+        break;
+    case 2:
+        printf("howareyou");
+        break;
+    default:
+        printf("Invalid value in ENUMERATED ((*pData))");
+    }
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1MyBool(const char *paramName, const asn1SccMyBool *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s MyBool ::= ", paramName);
+    printf("%s ", paramName);
+    printf("%s", (int)(*pData)?"TRUE":"FALSE");
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1MyReal(const char *paramName, const asn1SccMyReal *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s MyReal ::= ", paramName);
+    printf("%s ", paramName);
+    printf("%f", (*pData));
 #endif
 #ifdef __linux__
     pthread_mutex_unlock(&g_printing_mutex);
@@ -175,48 +138,6 @@ void PrintASN1MyChoice(const char *paramName, const asn1SccMyChoice *pData)
 #endif
 }
 
-void PrintASN1MyReal(const char *paramName, const asn1SccMyReal *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    //printf("%s MyReal ::= ", paramName);
-    printf("%s ", paramName);
-    printf("%f", (*pData));
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintASN1MyEnum(const char *paramName, const asn1SccMyEnum *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    //printf("%s MyEnum ::= ", paramName);
-    printf("%s ", paramName);
-    switch((*pData)) {
-    case 0:
-        printf("hello");
-        break;
-    case 1:
-        printf("world");
-        break;
-    case 2:
-        printf("howareyou");
-        break;
-    default:
-        printf("Invalid value in ENUMERATED ((*pData))");
-    }
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
 void PrintASN1MySeqOf(const char *paramName, const asn1SccMySeqOf *pData)
 {
 #ifdef __linux__
@@ -253,6 +174,47 @@ void PrintASN1MySeqOf(const char *paramName, const asn1SccMySeqOf *pData)
 #endif
 }
 
+void PrintASN1MySeq(const char *paramName, const asn1SccMySeq *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s MySeq ::= ", paramName);
+    printf("%s ", paramName);
+    printf("{");
+    printf("input-data ");
+    #if WORD_SIZE==8
+    printf("%lld", (*pData).input_data);
+    #else
+    printf("%d", (*pData).input_data);
+    #endif
+    printf(", ");
+    printf("output-data ");
+    #if WORD_SIZE==8
+    printf("%lld", (*pData).output_data);
+    #else
+    printf("%d", (*pData).output_data);
+    #endif
+    printf(", ");
+    printf("validity ");
+    switch((*pData).validity) {
+    case 0:
+        printf("valid");
+        break;
+    case 1:
+        printf("invalid");
+        break;
+    default:
+        printf("Invalid value in ENUMERATED ((*pData).validity)");
+    }
+    printf("}");
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
 void PrintASN1T_UInt8(const char *paramName, const asn1SccT_UInt8 *pData)
 {
 #ifdef __linux__
@@ -260,6 +222,59 @@ void PrintASN1T_UInt8(const char *paramName, const asn1SccT_UInt8 *pData)
 #endif
 #ifdef __unix__
     //printf("%s T-UInt8 ::= ", paramName);
+    printf("%s ", paramName);
+    #if WORD_SIZE==8
+    printf("%lld", (*pData));
+    #else
+    printf("%d", (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1T_Int32(const char *paramName, const asn1SccT_Int32 *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s T-Int32 ::= ", paramName);
+    printf("%s ", paramName);
+    #if WORD_SIZE==8
+    printf("%lld", (*pData));
+    #else
+    printf("%d", (*pData));
+    #endif
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1T_Boolean(const char *paramName, const asn1SccT_Boolean *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s T-Boolean ::= ", paramName);
+    printf("%s ", paramName);
+    printf("%s", (int)(*pData)?"TRUE":"FALSE");
+#endif
+#ifdef __linux__
+    pthread_mutex_unlock(&g_printing_mutex);
+#endif
+}
+
+void PrintASN1MyInteger(const char *paramName, const asn1SccMyInteger *pData)
+{
+#ifdef __linux__
+    pthread_mutex_lock(&g_printing_mutex);
+#endif
+#ifdef __unix__
+    //printf("%s MyInteger ::= ", paramName);
     printf("%s ", paramName);
     #if WORD_SIZE==8
     printf("%lld", (*pData));
@@ -285,21 +300,6 @@ void PrintASN1T_Int8(const char *paramName, const asn1SccT_Int8 *pData)
     #else
     printf("%d", (*pData));
     #endif
-#endif
-#ifdef __linux__
-    pthread_mutex_unlock(&g_printing_mutex);
-#endif
-}
-
-void PrintASN1MyBool(const char *paramName, const asn1SccMyBool *pData)
-{
-#ifdef __linux__
-    pthread_mutex_lock(&g_printing_mutex);
-#endif
-#ifdef __unix__
-    //printf("%s MyBool ::= ", paramName);
-    printf("%s ", paramName);
-    printf("%s", (int)(*pData)?"TRUE":"FALSE");
 #endif
 #ifdef __linux__
     pthread_mutex_unlock(&g_printing_mutex);
