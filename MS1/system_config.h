772e63214f26e22848a2d38db822cdf843a1f3e8
mdashnyam <muugii02@gmail.com>
2017-06-14 12:02:14 +0200

    Adding mission sensor data

---
+++ MS1/system_config.h
@@ -0,0 +1,14 @@
+/* This file was generated automatically - DO NOT MODIFY IT ! */
+
+/* Configuration file used by C_ASN1_Types.h */
+
+#define __NEED_T_UInt32_NATIVE
+#define __NEED_T_UInt32_NATIVE
+#define __NEED_MyInteger_NATIVE
+#define __NEED_MyInteger_UPER
+#define __NEED_T_UInt32_NATIVE
+#define __NEED_T_UInt32_NATIVE
+#define __NEED_MyInteger_NATIVE
+#define __NEED_MyReal_UPER
+#define __NEED_MyReal_UPER
+#define __NEED_MyInteger_UPER
---
+++ MS1/visualizer/visualizer.h
@@ -0,0 +1,25 @@
+/* This file was generated automatically: DO NOT MODIFY IT ! */
+
+/* Declaration of the functions that have to be provided by the user */
+
+#ifndef __USER_CODE_H_visualizer__
+#define __USER_CODE_H_visualizer__
+
+#include "C_ASN1_Types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void visualizer_startup();
+
+void visualizer_PI_display(const asn1SccMyReal *);
+
+extern void visualizer_RI_talk_with_MSSV();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
