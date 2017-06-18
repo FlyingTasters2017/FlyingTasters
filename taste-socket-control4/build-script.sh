c93c2c21a82b884faab387358656f604caaf9c6b
mdashnyam <taste@tuetaste.tue.nl>
2017-06-18 17:06:35 +0200

    New project 4 with Camera supervisor and drone communication inside it

---
+++ taste-socket-control4/build-script.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+# This script will build your TASTE system (by default with the C runtime).
+
+# You should not change this file as it was automatically generated.
+
+# If you need additional preprocessing, create a file named 'user_init_pre.sh'
+# and/or 'user_init_post.sh - They will never get overwritten.'
+
+# Inside these files you may set some environment variables:
+#    C_INCLUDE_PATH=/usr/include/xenomai/analogy/:${C_INCLUDE_PATH}
+#    unset USE_POHIC   
+
+CWD=$(pwd)
+
+if [ -t 0 ] ; then
+    COLORON="\e[1m\e[32m"
+    COLOROFF="\e[0m"
+else
+    COLORON=""
+    COLOROFF=""
+fi
+INFO="${COLORON}[INFO]${COLOROFF}"
+
+if [ -f user_init_pre.sh ]
+then
+    echo -e "${INFO} Executing user-defined init script"
+    source user_init_pre.sh
+fi
+
+# Use PolyORB-HI-C runtime
+USE_POHIC=1
+
+# Detect models from Ellidiss tools v2, and convert them to 1.3
+INTERFACEVIEW=InterfaceView.aadl
+grep "version => \"2" InterfaceView.aadl >/dev/null && {
+    echo -e "${INFO} Converting interface view from V2 to V1.3"
+    TASTE --load-interface-view InterfaceView.aadl --export-interface-view-to-1_3 __iv_1_3.aadl
+    INTERFACEVIEW=__iv_1_3.aadl
+};
+
+if [ -z "$DEPLOYMENTVIEW" ]
+then
+    DEPLOYMENTVIEW=DeploymentView.aadl
+fi
+
+# Detect models from Ellidiss tools v2, and convert them to 1.3
+grep "version => \"2" "$DEPLOYMENTVIEW" >/dev/null && {
+    echo -e "${INFO} Converting deployment view from V2 to V1.3"
+    TASTE --load-deployment-view "$DEPLOYMENTVIEW" --export-deployment-view-to-1_3 __dv_1_3.aadl
+    DEPLOYMENTVIEW=__dv_1_3.aadl
+};
+
+SKELS="./"
+
+# Update the data view with local paths
+taste-update-data-view
+
+cd "$SKELS" && rm -f socketclient.zip && zip socketclient socketclient/* && cd $OLDPWD
+
+cd "$SKELS" && rm -f pixyprocess.zip && zip pixyprocess pixyprocess/* && cd $OLDPWD
+
+cd "$SKELS" && rm -f cclient.zip && zip cclient cclient/* && cd $OLDPWD
+
+cd "$SKELS" && rm -f timer.zip && zip timer timer/* && cd $OLDPWD
+
+cd "$SKELS" && rm -f pixycam.zip && zip pixycam pixycam/* && cd $OLDPWD
+
+[ ! -z "$CLEANUP" ] && rm -rf binary*
+
+if [ -f ConcurrencyView.pro ]
+then
+    ORCHESTRATOR_OPTIONS+=" -w ConcurrencyView.pro "
+fi
+
+if [ -f user_init_post.sh ]
+then
+    echo -e "${INFO} Executing user-defined post-init script"
+    source user_init_post.sh
+fi
+
+if [ ! -z "$USE_POHIC" ]
+then
+    OUTPUTDIR=binary.c
+    ORCHESTRATOR_OPTIONS+=" -p "
+elif [ ! -z "$USE_POHIADA" ]
+then
+    OUTPUTDIR=binary.ada
+else
+    OUTPUTDIR=binary
+fi
+
+cd "$CWD" && assert-builder-ocarina.py \
+	--fast \
+	--debug \
+	--aadlv2 \
+	--keep-case \
+	--interfaceView "$INTERFACEVIEW" \
+	--deploymentView "$DEPLOYMENTVIEW" \
+	-o "$OUTPUTDIR" \
+	--subC socketclient:"$SKELS"/socketclient.zip \
+	--subC pixyprocess:"$SKELS"/pixyprocess.zip \
+	--subC cclient:"$SKELS"/cclient.zip \
+	--subC timer:"$SKELS"/timer.zip \
+	--subC pixycam:"$SKELS"/pixycam.zip \
+	$ORCHESTRATOR_OPTIONS
+
+if [ -f user_init_last.sh ]
+then
+    echo -e "${INFO} Executing user-defined post-build script"
+    source user_init_last.sh
+fi
+
---
+++ taste-socket-control4/pixycam/pixycam.c
@@ -0,0 +1,143 @@
+/* User code: This file will not be overwritten by TASTE. */
+
+#include "pixycam.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pixy.h"
+#include <stdbool.h>
+
+#include <inttypes.h>
+#include <math.h>
+#include <time.h>
+
+#define _POSIX_C_SOURCE 200809L
+
+
+#define BLOCK_BUFFER_SIZE    25
+int pixy_init_status;
+
+void pixycam_startup()
+{
+    /* Write your initialization code here,
+       but do not make any call to a required interface. */
+    pixy_taste_init();
+    
+}
+
+void printTime() {
+    long            ms; // Milliseconds
+    time_t          s;  // Seconds
+    struct timespec spec;
+
+    clock_gettime(CLOCK_REALTIME, &spec);
+
+    s  = spec.tv_sec;
+    ms = round(spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
+
+    printf( " Current time: % " PRIdMAX ".%03ld seconds since the Epoch\n ",
+           (intmax_t)s, ms);
+}
+
+void pixy_taste_init()
+{
+    // Connect to Pixy //
+    pixy_close();
+    {
+        pixy_init_status = pixy_init();
+        printf("pixy_init(): ");
+        pixy_error(pixy_init_status);
+    }
+    
+    // Request Pixy firmware version //
+    {
+        uint16_t major;
+        uint16_t minor;
+        uint16_t build;
+        int      return_value;
+
+        return_value = pixy_get_firmware_version(&major, &minor, &build);
+
+        if (return_value) {
+            // Error //
+            printf("Failed to retrieve Pixy firmware version. ");
+            pixy_error(return_value);
+
+            //return return_value;
+            } 
+        else {
+            // Success //
+            printf(" Pixy Firmware Version: %d.%d.%d\n", major, minor, build);
+        }
+    }
+}
+void pixycam_PI_pulse()
+{
+    printf("Start pixycam_PI_rawdata()\n");
+    printTime();    
+    // Pixy Block buffer // 
+        struct Block blocks[BLOCK_BUFFER_SIZE];
+
+
+        static bool run_flag = true;
+        
+        //int      i;        
+        int      index;
+        int      blocks_copied;
+        char     buf[128];
+        asn1SccMyDroneData droneData;
+        asn1SccMySensorData sensorData;
+
+        // Catch CTRL+C (SIGINT) signals //
+        //signal(SIGINT, handle_SIGINT);
+        //printf("Hello Pixy:\n libpixyusb Version: %s\n", __LIBPIXY_VERSION__);
+
+        
+        // Was there an error initializing pixy? //
+        if(!pixy_init_status == 0)
+        {
+            // Error initializing Pixy //
+            printf("pixy_init(): ");
+            pixy_error(pixy_init_status);
+
+            //return pixy_init_status;
+        }
+        
+        printf("Detecting blocks...\n");
+        
+        asn1SccT_UInt32 x;
+        asn1SccT_UInt32 y;
+        pixycam_RI_takeoff(&droneData);
+        
+        while(run_flag)
+
+        {
+            printf("Start cycle\n");
+            printTime();
+            // Wait for new blocks to be available //
+            while(!pixy_blocks_are_new() /*&& run_flag*/); 
+            // Get blocks from Pixy //
+            blocks_copied = pixy_get_blocks(BLOCK_BUFFER_SIZE, &blocks[0]);
+            if(blocks_copied < 0) {
+                // Error: pixy_get_blocks //
+                printf("pixy_get_blocks(): ");
+                pixy_error(blocks_copied);
+            }
+
+            // Display received blocks //
+            printf("Frame :\n");
+            for(index = 0; index != blocks_copied; ++index) 
+            {    
+                sprintf(buf, "CC block! (%d decimal) x: %d y: %d width: %d height: %d angle %d", blocks[index].signature, blocks[index].x, blocks[index].y, blocks[index].width, blocks[index].height, blocks[index].angle);
+                printf("  %s\n", buf);
+                x=blocks[index].x;
+                y=blocks[index].y;
+                pixycam_RI_processData(&x, &y);
+            }
+            printf("starting drone comm:\n");
+            printTime();
+            pixycam_RI_readStabilizerSendThrust(&droneData,&sensorData);
+        }
+        printf("end of pixycam_PI_rawdata()\n");
+        printTime();
+}
